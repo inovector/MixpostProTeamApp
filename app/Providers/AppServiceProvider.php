2fb2d4f5d5bf8e2d19103c43ae5a93cdfa865d94
Dumitru Botezatu <dima.botezatu@gmail.com>
2024-04-15 22:08:56 +0300

    added sentry

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Inovector\Mixpost\Mixpost;
+use Sentry\Laravel\Integration;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Mixpost::report(function($exception) {
+            Integration::captureUnhandledException($exception);
+        });
     }
 
     /**
